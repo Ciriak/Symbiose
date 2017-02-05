744d75a3caedaf3b3bc7d2d55ffde4a9e42173dd
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2017-02-05 23:10:57 +0100

    //WIP child process for wallpaper processing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 var electron = require('electron');
-var {app, Menu, Tray} = require('electron');
-var {webContents} = require('electron')
+var {app, Menu, Tray, webContents} = require('electron');
 var Menu = electron.Menu;
 var BrowserWindow = electron.BrowserWindow;
 var GhReleases = require('electron-gh-releases');
@@ -22,6 +21,7 @@ var screens;
 //retreive package.json properties
 var pjson = require('./package.json');
 var sources = require('./sources.json');
+
 //Settings file (default)
 var settings = {
   local: {
@@ -822,90 +822,17 @@ ipc.on('installUpdate', function (fileData) {
 function createWallpaper(wallpapers, screens, callback){
 
   //spawn a new process (prevent freeze)
-  var wpProcess = spawn('wallpaper.js', 'bar', 'baz', {
-    detached: true
+  var wallpaperProcess = childProcess.fork('wallpaper.js', {
+    cwd: __dirname
   });
-
-  var stacks = [];
-  var frame = {
-    width: 0,
-    height: 0,
-    offsetX: 0,
-    offsetY: 0
-  };
-  //define the size of the "final" image
-  var oOffsetX = frame.offsetX;
-  var oOffsetY = frame.offsetY;
-  for (var i = 0; i < screens.length; i++) {
-    frame.width += screens[i].size.width;
-    if(screens[i].size.height > frame.height){
-      frame.height = screens[i].size.height;
-    }
-    if(screens[i].bounds.x < oOffsetX){
-      frame.offsetX = Math.abs(screens[i].bounds.x);
-    }
-    if(screens[i].bounds.y < oOffsetY){
-      frame.offsetY = Math.abs(screens[i].bounds.y);
-    }
-  }
-
-  console.log("Creating a wallpaper with "+screens.length+" frames from :");
-  for (i = 0; i < screens.length; i++) {
-    var tv = wallpapers[0];
-    stacks.push(createWallpaperFrame(screens[i], wallpapers[0], i));
-
-    //send the first item to the last position
-    wallpapers.splice(0, 1);
-    wallpapers.push(tv);
-  }
-
-  async.parallel(stacks, function(err, images) {
-    if(err){
-      console.log(err);
-      return callback(err);
-    }
-
-    console.log("Assembing...");
-    var generated = new Jimp(frame.width, frame.height, function (err, generated) {
-      if(err){
-        console.log(err);
-        return callback(err);
-      }
-      for (var i = 0; i < images.length; i++) {
-        var x = screens[i].bounds.x+frame.offsetX;
-        var y = screens[i].bounds.y+frame.offsetY;
-        generated.composite( images[i], x, y );
-      }
-      console.log("Saving the generated image...");
-      generated.write(tempDir+"\\wallpaper.jpg", function(r){
-        console.log(r);
-        nodeWallpaper.set(tempDir+"\\wallpaper.jpg");
-        console.log("...done");
-        return callback();
-      });
-    });
-
+  wallpaperProcess.send("dddd");
+  wallpaperProcess.on('message', function(e){
+    console.log(e);
+  });
+  wallpaperProcess.on('exit', function (code, signal) {
+    console.log('wallpaper Process exited:', code, signal);
   });
 }
-
-var createWallpaperFrame = function(screen, wallpaper, index, callback){
-  console.log(wallpaper.localUri);
-  var u = wallpaper.localUri.replace('%localDir%', settings.local.syncedPath);
-  return function(callback){
-    Jimp.read(u, function (err, image) {
-      if(err){
-        console.log(err);
-        return callback(err, null);
-      }
-      image.cover(screen.size.width, screen.size.height);
-      console.log("Frame "+index+" ready.");
-      image.write(tempDir+"\\frame_"+index+".jpg", function(r, e){
-        return callback(null, image);
-      });
-
-    });
-  };
-};
 
 function rmDir(dirPath, removeSelf) {
   if (removeSelf === undefined)
---
+++ src/wallpaper.js
@@ -0,0 +1,86 @@
+console.log('Wallpaper process spawned !');
+var stacks = [];
+var frame = {
+  width: 0,
+  height: 0,
+  offsetX: 0,
+  offsetY: 0
+};
+
+//error reporting
+process.on('uncaughtException', function (err) {
+  console.log(err);
+});
+
+//define the size of the "final" image
+var oOffsetX = frame.offsetX;
+var oOffsetY = frame.offsetY;
+for (var i = 0; i < screens.length; i++) {
+  frame.width += screens[i].size.width;
+  if(screens[i].size.height > frame.height){
+    frame.height = screens[i].size.height;
+  }
+  if(screens[i].bounds.x < oOffsetX){
+    frame.offsetX = Math.abs(screens[i].bounds.x);
+  }
+  if(screens[i].bounds.y < oOffsetY){
+    frame.offsetY = Math.abs(screens[i].bounds.y);
+  }
+}
+
+console.log("Creating a wallpaper with "+screens.length+" frames from :");
+for (i = 0; i < screens.length; i++) {
+  var tv = wallpapers[0];
+  stacks.push(createWallpaperFrame(screens[i], wallpapers[0], i));
+
+  //send the first item to the last position
+  wallpapers.splice(0, 1);
+  wallpapers.push(tv);
+}
+
+async.parallel(stacks, function(err, images) {
+  if(err){
+    console.log(err);
+    return callback(err);
+  }
+
+  console.log("Assembing...");
+  var generated = new Jimp(frame.width, frame.height, function (err, generated) {
+    if(err){
+      console.log(err);
+      return callback(err);
+    }
+    for (var i = 0; i < images.length; i++) {
+      var x = screens[i].bounds.x+frame.offsetX;
+      var y = screens[i].bounds.y+frame.offsetY;
+      generated.composite( images[i], x, y );
+    }
+    console.log("Saving the generated image...");
+    generated.write(tempDir+"\\wallpaper.jpg", function(r){
+      console.log(r);
+      nodeWallpaper.set(tempDir+"\\wallpaper.jpg");
+      console.log("...done");
+      return callback();
+    });
+  });
+
+});
+
+var createWallpaperFrame = function(screen, wallpaper, index, callback){
+  console.log(wallpaper.localUri);
+  var u = wallpaper.localUri.replace('%localDir%', settings.local.syncedPath);
+  return function(callback){
+    Jimp.read(u, function (err, image) {
+      if(err){
+        console.log(err);
+        return callback(err, null);
+      }
+      image.cover(screen.size.width, screen.size.height);
+      console.log("Frame "+index+" ready.");
+      image.write(tempDir+"\\frame_"+index+".jpg", function(r, e){
+        return callback(null, image);
+      });
+
+    });
+  };
+};
